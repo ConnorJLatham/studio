b1b2555ad54ca3066dd0a94e72ae225b559c9519
ConnorJLatham <connor.lathampri@gmail.com>
2020-09-13 01:58:02 -0700

    adding the design work i have so far

---
+++ command_handler.py
@@ -0,0 +1,12 @@
+class Command:
+    transmitter = ''
+    intended_receiver = ''
+    time_stamp = ''
+
+class CommandHandler:
+    def pass_command(self, command):
+        pass
+    def check_command(self, command):
+        pass
+    def reject_command(self, command):
+        
